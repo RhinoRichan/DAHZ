d4c68078e5a19fe395ecb063b1027061abbb0320
Ikhsan Saleh <ikhsan.ichan.maulana@gmail.com>
2015-07-03 16:52:49 +0700

    css action to enqueue control scripts

--- customizer/dahz-customize-scripts.php
+++ customizer/dahz-customize-scripts.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 if ( ! defined( 'ABSPATH' ) ) {
   exit;
 }
 
 /**
-* get style and script 
+* get style and script
 * @author Dahz
 * @since  1.5.0
 */
@@ -17,20 +17,17 @@ class Dahz_Customizer_Scripts {
 
     $this->suffix = dahz_get_min_suffix();
 
-    add_action('customize_controls_print_styles', array($this, 'EnqueueStyles'));
     add_action('customize_controls_enqueue_scripts', array($this, 'EnqueueScripts'));
 
     //register custom control scripts
     add_action('customize_controls_enqueue_scripts', array($this, 'customize_controls_register_script'), 5);
   }
 
-  function EnqueueStyles() {
+  function EnqueueScripts() {
       wp_enqueue_style('dahz-customizer', DF_CORE_CSS_DIR . 'dahz-customizer'. $this->suffix .'.css');
       //HOOK
       do_action('dahz_enqueue_customizer_admin');
-  }
 
-  function EnqueueScripts() {
       wp_enqueue_script('dahz-semantic-ui', DF_CORE_JS_DIR . 'semantic.js', array('jquery', 'customize-controls'), false, true );
       wp_enqueue_script('dahz-customizer-main', DF_CORE_JS_DIR . 'customizer-main'. $this->suffix .'.js', array('jquery', 'customize-controls'), false, true );
   }
