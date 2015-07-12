a48d1cc3f12b5d03f5266575f1bbaf21dd54b59b
Ikhsan Saleh <ikhsan.ichan.maulana@gmail.com>
2015-07-12 13:57:56 +0700

    remove render content method

--- customizer/controls/text/text-subtitle-custom-control.php
+++ customizer/controls/text/text-subtitle-custom-control.php
@@ -8,13 +8,10 @@ if ( ! class_exists( 'WP_Customize_Control' ) )
  * @since 1.2.0
  */
  class DAHZ_Subtitle_Control extends WP_Customize_Control {
-  
+
      public $type = 'sub-title';
-       /**
-       * Render the content on the theme customizer page
-       */
-     public function render_content() {}
-    
+
+
      public function content_template() {
      ?>
       <h4 class="customize-subtitle">{{ data.label }}</h4>
