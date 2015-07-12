9c0cfa7964c5721c2e35f0aa495a5753b4f8c6f8
Ikhsan Saleh <ikhsan.ichan.maulana@gmail.com>
2015-07-13 00:33:33 +0700

    using class css to create mode

--- customizer/controls/text/checkbox-custom-control.php
+++ customizer/controls/text/checkbox-custom-control.php
@@ -24,24 +24,16 @@ class DAHZ_Checkbox_Control extends WP_Customize_Control {
 	public function render_content() {
 		?>
 
-		<?php if ( 'toggle' == $this->mode ) : ?>
+		<?php $modeClass = ( 'toggle' == $this->mode ) ? 'ui toggle checkbox' : 'ui' ; ?>
 
-		<div class="ui toggle checkbox">
+		<div class="<?php echo $modeClass; ?>">
    		 	<input type="checkbox" id="<?php echo esc_attr($this->id); ?>" name="public" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); checked( esc_attr( $this->value() ) );?> >
-    		<label for="<?php echo esc_attr($this->id); ?>"><?php echo esc_html( $this->label ); ?></label>
-  		</div>
+    		<label for="<?php echo esc_attr( $this->id ); ?>"><?php echo esc_html( $this->label ); ?></label>
+				<?php if ( ! empty( $this->description ) ) : ?>
+					<span class="description customize-control-description"><?php echo $this->description; ?></span>
+				<?php endif; ?>
+  	</div>
 
-		<?php else : ?>
-
-		<label>
-			<input type="checkbox" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); checked( esc_attr( $this->value() ) ); ?> />
-			<?php echo esc_html( $this->label ); ?>
-			<?php if ( ! empty( $this->description ) ) : ?>
-				<span class="description customize-control-description"><?php echo $this->description; ?></span>
-			<?php endif; ?>
-		</label>
-
-  	   <?php endif ?>
 
 		<?php
 	}
